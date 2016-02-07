1569b7e67faa5013ba0158e44a8441ac9c14d7f0
Bienvenido Rodriguez <bienvenidocrodriguez@gmail.com>
2016-02-07 14:56:30 -0500

    Added conversion between f and c

---
+++ Intro_to_Programming/ConvertCtoF.py
@@ -0,0 +1,7 @@
+# A program that converts Celsius to Farenheit
+
+def convertC2F(x):
+    z = (x * 1.8 + 32)
+    print(z)
+
+convertC2F(38.05555555555556)
---
+++ Intro_to_Programming/ConvertFtoC.py
@@ -0,0 +1,7 @@
+# A program that converts farenheit to celsius
+
+def convertF2C(x):
+    cel = ((x - 32) / 1.8)
+    print(cel)
+    
+convertF2C(100.5)
