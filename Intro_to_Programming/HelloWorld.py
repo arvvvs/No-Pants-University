afe2ab7ea1c951555cc296a8178fe4a286bd3dfd
Bienvenido Rodriguez <bienvenidocrodriguez@gmail.com>
2016-02-07 14:20:40 -0500

    Updated HelloWorld.py

--- Intro_to_Programming/HelloWorld.py
+++ Intro_to_Programming/HelloWorld.py
@@ -1,9 +1,21 @@
 # Hello World Program
+
 # Function: main() - Print out Hello World
 # If I don't put main() at the end, it will compile I guess but not give me an output
 
 
-def hello():
+def main():
     print("Hello, World!")
     
-hello()
+main()
+
+# So, the main() is the invokation which is needed to call our function
+# Can be done with a different function name
+# Example: We can do
+
+#def hello():
+#    print("Hello, World!")
+#hello()
+
+
+# This will function the same way as the main() one
