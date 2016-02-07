45c9dc5964eb2e87930006dbbca208fa8f6b6800
Bienvenido Rodriguez <bienvenidocrodriguez@gmail.com>
2016-02-07 14:18:11 -0500

    Changed README and Updated HelloWorld.py

---
+++ Intro_to_Programming/HelloWorld.py
@@ -0,0 +1,9 @@
+# Hello World Program
+# Function: main() - Print out Hello World
+# If I don't put main() at the end, it will compile I guess but not give me an output
+
+
+def hello():
+    print("Hello, World!")
+    
+hello()
--- Intro_to_Programming/README.md
+++ Intro_to_Programming/README.md
@@ -7,7 +7,7 @@
 * What is a programming 
 * What is a bug?
 * Types in Python
+* Functions
 * Numeric Precision in Python
 * How does a machine run a Python program?
 * Memory model
-* Functions
