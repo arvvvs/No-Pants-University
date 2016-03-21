5560ebe07b459a9fcde1a8830ebc3a81efca5aee
Bienvenido Rodriguez <bienvenidocrodriguez@gmail.com>
2016-03-20 20:50:08 -0400

    Deleted executables, classes, etc.

---
+++ IntroToComputerScience/Java/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+    Ben Rodriguez
+    Hello World program in Java
+*/
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
